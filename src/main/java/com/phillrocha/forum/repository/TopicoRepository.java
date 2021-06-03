80f9b55679142603de7bde6da9506fb81e30ab84
Phillippe Guedes <phillippe.guedes@hp.com>
2021-06-03 19:10:00 -0300

    Adding repository

--- src/main/java/com/phillrocha/forum/repository/TopicoRepository.java
+++ src/main/java/com/phillrocha/forum/repository/TopicoRepository.java
@@ -3,6 +3,6 @@ package com.phillrocha.forum.repository;
 import com.phillrocha.forum.models.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TopicoRepository extends JpaRepository<Topico, Long> {
+    public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
 }
