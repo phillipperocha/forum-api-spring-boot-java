b16b525dc1e2dc2c545fda2edeed26fb305f9383
Phillippe Guedes <phillippe.guedes@hp.com>
2021-06-13 18:42:59 -0300

    Simplifying paging and ordering

--- src/main/java/com/phillrocha/forum/controller/TopicosController.java
+++ src/main/java/com/phillrocha/forum/controller/TopicosController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -44,12 +45,10 @@ public class TopicosController {
 
     @GetMapping
     public Page<TopicoDto> list(@RequestParam(required = false) String tituloTopico,
-            @RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd,
-            @RequestParam String ordenacao) {
+                                @RequestParam(required = false) String nomeCurso,
+                                @PageableDefault(sort="id", direction=Direction.ASC, page = 0, size = 10) Pageable paginacao) {
 
         Page<Topico> topicos;
-
-        Pageable paginacao = PageRequest.of(pagina, qtd, Direction.ASC, ordenacao);
 
         if (nomeCurso == null && tituloTopico == null) {
             topicos = topicoRepository.findAll(paginacao);
