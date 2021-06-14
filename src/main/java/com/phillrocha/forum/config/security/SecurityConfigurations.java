b7b87a1499a26240c412738b430c364531996f9b
Phillippe Guedes <phillippe.guedes@hp.com>
2021-06-13 21:08:24 -0300

    Openning access to some endpoints

--- src/main/java/com/phillrocha/forum/config/security/SecurityConfigurations.java
+++ src/main/java/com/phillrocha/forum/config/security/SecurityConfigurations.java
@@ -1,10 +1,31 @@
 package com.phillrocha.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    // Authentication Config
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    }
+
+    // Authorization Config
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/topics").permitAll()
+        .antMatchers(HttpMethod.GET, "/topics/*").permitAll();
+    }
+
+    // Static Files Config
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+    }
 }
