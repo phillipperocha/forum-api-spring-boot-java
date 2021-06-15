4775cf48345c314772c81ebab57ec300f530d0d1
Phillippe Rocha <phillipperocha@gmail.com>
2021-06-14 21:26:41 -0300

    Authenticating users

---
+++ src/main/java/com/phillrocha/forum/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.phillrocha.forum.repository;
+
+import com.phillrocha.forum.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+}
